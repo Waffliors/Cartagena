43e3716e4df3a945784b7e0f6f732bb3b95e14a4
RaphaelO-Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2019-11-22 11:16:50 -0300

    Filtro de String implementada
    Nome de usuário n pode conter espaço, caracteres especiais e nem passar de 8 letras

--- app/src/main/java/com/example/cartagena_waffliors/PreMainActivity.java
+++ app/src/main/java/com/example/cartagena_waffliors/PreMainActivity.java
@@ -1,29 +1,67 @@
 package com.example.cartagena_waffliors;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.view.View;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//Classe que coleta e filtra o nome do usuário para utilizar na aplicação
 public class PreMainActivity extends AppCompatActivity {
-    EditText nomeUsuario;
-    Button botaoIniciaJogo;
+    Button   startGameButton;
+    EditText userName;
 
+    //Na criação da activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_main);
-
-        nomeUsuario = findViewById(R.id.userNameText);
-        botaoIniciaJogo = findViewById(R.id.buttonStartGame);
-        botaoIniciaJogo.setOnClickListener(new View.OnClickListener() {
+        //Bind dos elementos da activity
+        userName = findViewById(R.id.userNameText);
+        startGameButton = findViewById(R.id.buttonStartGame);
+        //Adiciona listener no botão
+        startGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                String userNameString = userName.getText().toString();
+                stringFilter(userNameString);
             }
         });
+    }
+
+    //Filtro de String
+    private boolean stringFilter(String strng){
+        //Filtra espaço na String
+        if(strng.contains(" ")){
+            Toast.makeText(this.getApplicationContext(),
+                    "O nome não pode ter espaços"
+                    ,Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        //Filtra pelo tamanho da String
+        if(strng.length() > 8){
+            Toast.makeText(this.getApplicationContext(),"O nome deve ter menos de 8 caracteres"
+                    ,Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        //Filtra os caracteres especiais
+        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(strng);
+        boolean b = m.find();
+        if (b) {
+            Toast.makeText(this.getApplicationContext(),
+                    "O nome só pode ser formado por letras e números"
+                    ,Toast.LENGTH_LONG).show();
+            return false;
+        }
+        //Se passou nos dois filtros, é aprovado
+        return true;
     }
 }
