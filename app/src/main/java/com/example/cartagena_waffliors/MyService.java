bdb8ddeb570229dc3e6ba77973b454035b5f35bc
MatthewVicente <41347019+MatthewVicente@users.noreply.github.com>
2019-12-04 20:26:46 -0300

    Start to get map

--- app/src/main/java/com/example/cartagena_waffliors/MyService.java
+++ app/src/main/java/com/example/cartagena_waffliors/MyService.java
@@ -12,4 +12,7 @@ public interface MyService {
     
     @GET("rest/v1/jogador/mao/{idJogador}/{senhaJogador}")
     public Call<Carta[]> pegaCartasJogador(@Path("idJogador") String idPartida, @Path("senhaJogador") String senhaJogador);
+
+    @GET("rest/v1/jogo/tabuleiro/{idPartida}")
+    public Call<Tile[]> pegaTiles(@Path("idPartida") String idPartida);
 }
---
+++ app/src/main/java/com/example/cartagena_waffliors/Tile.java
@@ -0,0 +1,28 @@
+package com.example.cartagena_waffliors;
+
+public class Tile {
+
+    private int posicao;
+    private int qntd;
+    private String tipo;
+
+    public int getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(int posicao) {
+        this.posicao = posicao;
+    }
+
+    public int getQntd() {
+        return qntd;
+    }
+
+    public void setQntd(int qntd) {
+        this.qntd = qntd;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
