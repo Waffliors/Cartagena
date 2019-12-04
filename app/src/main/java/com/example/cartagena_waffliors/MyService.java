97634a0cbeb9cc789ce059e1a2a565d012823d0f
Raphael Oliveira Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2019-12-04 20:18:11 -0300

    Coletando status do jogo em loop

--- app/src/main/java/com/example/cartagena_waffliors/MyService.java
+++ app/src/main/java/com/example/cartagena_waffliors/MyService.java
@@ -12,4 +12,7 @@ public interface MyService {
     
     @GET("rest/v1/jogador/mao/{idJogador}/{senhaJogador}")
     public Call<Carta[]> pegaCartasJogador(@Path("idJogador") String idPartida, @Path("senhaJogador") String senhaJogador);
+
+    @GET("rest/v1/jogo/status/{idPartida}")
+    public Call<Status>pegaStatusPartida(@Path("idPartida")String idPartida);
 }
